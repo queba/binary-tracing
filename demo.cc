901d37a4ec8041ef17256399e62b301235e787f1
Yin Qiu <qiuyin@gmail.com>
2015-01-24 00:40:08 +0800

    Partially implemented section III ("Real Implementation").
    The encoding part has been done. More work to be done for the
    persistence and decoding part. Basically, this commit is still toy
    and not really usable.
    
    There are also some changes to and some improvements on the original
    idea, including:
    1. Some of the functions are made constexpr.
    2. I will probably use the boost::format string directly and give up
       using operator<<.
    3. Unique tags are now generated by a compile-time cityhash library.
       Bullets 2 and 3 combined allow us to get rid the "code generation"
       stage (which runs awk over the code before actually building the
       code).

---
+++ demo.cc
@@ -0,0 +1,9 @@
+#include "dyad.h"
+
+int main(int argc, char *argv[])
+{
+    TRACE("hello, %1% arguments!", argc);
+
+    return 0;
+}
+
---
+++ dyad.h
@@ -0,0 +1,175 @@
+#ifndef DYAD_H
+#define DYAD_H
+
+#include <consthash/cityhash64.hxx>
+#include <boost/format.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
+#include <cstdint>
+#include <cstring>
+
+template <typename T>
+class TraceDecoratedValue
+{
+public:
+    static const bool kHasDecode = true;
+
+    TraceDecoratedValue()
+    {}
+
+    constexpr explicit TraceDecoratedValue(const T *v)
+        : _ref(v)
+    {}
+
+    void encode(char *buf) const
+    {
+        const char *s = (const char*)_ref;
+        std::memcpy(buf, _ref, sizeof(T));
+    }
+
+    static const T &decode(const char *buf, size_t &sz)
+    {
+        sz = sizeof(T);
+        return *reinterpret_cast<const T *>(buf);
+    }
+
+    constexpr size_t size() const
+    {
+        return sizeof(T);
+    }
+
+private:
+    const T *_ref;
+};
+
+template <size_t N>
+class TraceDecoratedValue<char [N]>
+{
+public:
+    static const bool kHasDecode = false;
+
+    explicit TraceDecoratedValue(const char (&)[N])
+    {}
+
+    void encode(char *buf) const
+    {}
+
+    constexpr size_t size() const
+    {
+        return 0;
+    }
+};
+
+template <typename T, typename... TRest>
+constexpr size_t decorated_total_size(const T &x, const TRest &... val)
+{
+    return x.size() + decorated_total_size(val...);
+}
+
+template <typename T>
+constexpr size_t decorated_total_size(const T &t)
+{
+    return t.size();
+}
+
+template <typename T>
+constexpr TraceDecoratedValue<T> trace_decorate(const T &val)
+{
+    return TraceDecoratedValue<T>(&val);
+}
+
+template <uint64_t TraceTag>
+class TraceControl
+{
+// Functions for online tracing
+public:
+    // offset is used for assertion
+    template <typename T, typename... TRest>
+    static void trace_encode(char *buf, size_t len, size_t offset,
+            const TraceDecoratedValue<T> &v,
+            const TraceDecoratedValue<TRest> &... x)
+    {
+        v.encode(buf + offset);
+        trace_encode(buf, len, offset + v.size(), x...);
+    }
+
+    static void trace_encode(char *buf, size_t len, size_t offset)
+    {
+        assert(len == offset);
+    }
+
+    template <typename... T>
+    static void trace_impl(const TraceDecoratedValue<T> &...x)
+    {
+        constexpr size_t total_size = decorated_total_size(x...);
+        char buf[total_size];
+        trace_encode(buf, total_size, 0, x...);
+    }
+
+    template <typename... T>
+    static void trace(const char *fmt, const T &... x)
+    {
+        // add a new entry in the dyad section
+        trace_impl(trace_decorate(&print<T...>), trace_decorate(x)...);
+    }
+
+// Functions for offline decoding
+public:
+    template <typename... T>
+    static size_t print(std::ostream &os, const char *buf)
+    {
+        boost::format fmt = get_trace_format();
+        size_t sz = print_impl<T...>(fmt, buf);
+        os << fmt.str() << std::endl;
+        return sz;
+    }
+private:
+    static boost::format get_trace_format()
+    {
+        // Search through the dyad section to find the format string for a tag
+        return boost::format();
+    }
+
+    template <typename T>
+    static size_t decode_single(boost::format &fmt, const char *buf,
+            const std::true_type &decode_yes)
+    {
+        size_t sz;
+        const T &val = TraceDecoratedValue<T>::decode(buf, sz);
+        fmt = fmt % val;
+        return sz;
+    }
+
+    template <typename T>
+    static size_t decode_single(boost::format &fmt, const char *buf,
+            const std::false_type &decode_no)
+    {
+        return 0;
+    }
+
+    template <typename T>
+    static size_t print_impl(boost::format &fmt, const char *buf)
+    {
+        typedef TraceDecoratedValue<T> DecoratedType;
+        return decode_single<T>(fmt, buf,
+                std::integral_constant<bool, DecoratedType::kHasDecode>());
+    }
+
+    template <typename T, typename TNext, typename... TRest>
+    static size_t print_impl(boost::format &fmt, const char *buf)
+    {
+        size_t sz = print_impl<T>(fmt, buf);
+        return sz + print_impl<TNext, TRest...>(fmt, buf + sz);
+    }
+
+};
+
+#define TRACE(fmt, ...) \
+    const static char format ## __LINE__ [] __attribute__((section("dyad"))) = \
+        fmt; \
+    TraceControl<consthash::city64(__FILE__ ":" BOOST_PP_STRINGIZE(__LINE__))>::trace(fmt, ##__VA_ARGS__);
+
+#endif
